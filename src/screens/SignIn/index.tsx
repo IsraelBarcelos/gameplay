6bbc250721a72877da3a8af2cd545dbfe0c31378
Israel da Rosa Barcelos <israelbarcelos7@gmail.com>
2021-06-24 19:21:14 -0300

    gone a bit further into componentization, created a global scope por variables

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,21 +1,46 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput } from 'react-native';
+import React from 'react';
+import { 
+  View,
+  Text,
+  Image,
+  StatusBar
+} from 'react-native';
 
 import { styles } from './styles';
+import IllustrationImg from "../../assets/illustration.png";
+import { ButtonIcon } from '../../components/ButtonIcon';
 
 export function SignIn() {
-
-    const [text, setText] = useState("")
+  
 
     return(
-        <View style={styles.container}>
-            <Text>Um texto ai</Text>
-            <TextInput
-              onChangeText={setText}
-              style={styles.input} 
-            />
+      
+      <View style={styles.container}>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        <Image 
+          source={IllustrationImg} 
+          style={styles.image}
+          resizeMode='stretch'
+        />
 
-            <Text>Você digitou {text}</Text>
+        <View> style={styles.content}
+          <Text style={styles.title}>            
+            Organize {`\n`}
+            suas jogatinas{`\n`}
+            facilmente
+          </Text>
+
+          <Text style={styles.subtitle}>
+            Crie grupos para jogar seus games {`\n`}
+            favoritos com seus amigos
+          </Text>
+
+          <ButtonIcon />
         </View>
+      </View>
     )
 }
--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -1,4 +1,5 @@
 import { StyleSheet } from 'react-native'
+import { theme } from '../../global/styles/theme'
 
 
 
@@ -7,10 +8,27 @@ export const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+        backgroundColor: theme.colors.background
     },
-    input: {
-        height: 50,
-        width: 200,
-        borderBottomWidth: 3
+    
+    image: {
+        width: '100%',
+        height: 360
+    },
+    content: {
+        marginTop: -40,
+    },
+    title: {
+        color: theme.colors.heading,
+        textAlign: 'center',
+        fontSize: 40,
+        marginBottom: 16,
+    },
+    subtitle: {
+        color: theme.colors.heading,
+        fontSize: 15,
+        textAlign: 'center',
+        marginBottom: 64
     }
+
 })
